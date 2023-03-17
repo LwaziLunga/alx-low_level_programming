ee207bee121e3680e1663d87e121edfe5e36da95
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-17 14:51:58 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,17 @@
  */
 int main(void)
 {
-printf("size of a char : %zu byte(s)\n", sizeof(char));
-printf("size of an int : %zu byte(s)\n", sizeof(int)); 
-printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("size of long long int  : %zu byte(s)\n", sizeof(long long int));
-printf("sizeof float : %zu byte(s)\n", sizeof(float));
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf ("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf ("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b)); 
+printf ("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf ("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf ("sizeof float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 return(0);
 }
