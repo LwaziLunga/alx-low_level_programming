7f76cd5df4b3ab781324b2a89d3d9f987df9bfc5
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-17 20:08:20 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *  * main -Entry point
+ *   *
+ *    * Return: Always 0(success)
+ *     */
+
+int main(void)
+{
+int a = 0;
+
+while (a < 10)
+{
+putchar(a +'0');
+a++
+}
+
+
+putchar('\n');
+
+return (0);
+}
