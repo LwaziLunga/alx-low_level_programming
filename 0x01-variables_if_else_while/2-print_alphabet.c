bd7f45b2cf9d6deab13e09c74bd44205e9e603f9
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-17 18:14:21 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,21 +12,13 @@
 
 int main(void)
 {
-int n;
+char alphabets [26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if ((n % 10)> 5)
+for (i = 0; i < 26; i++)
 {
-printf("The last digit of %d is %d and is greater than 5\n", n, n % 10);
+putchar(alphabets[i]);
 }
-else if ((n % 5) < 6 && (n % 5) != 0)
-{
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-}
-else
-{
-printf("The last digit of %d is %d and is 0\n", n, n % 10);
-}
+putchar('\n');
 return (0);
 }
