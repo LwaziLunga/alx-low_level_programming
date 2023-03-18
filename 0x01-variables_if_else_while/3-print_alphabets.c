b2968942ad61072de3f9c56af7c5ceb315222765
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-17 18:23:16 -0700

    cap

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+char Alph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(alphabets[i]);
+}
+putchar('\n');
+
+for (i = 0; i < 26; i++)
+{
+putchar(Alph[i]);
+}
+putchar('\n');
+return (0);
+}
