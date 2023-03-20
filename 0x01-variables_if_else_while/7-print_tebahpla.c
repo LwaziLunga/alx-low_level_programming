2d58d1dfa5d0bbad42f2690e93199821fe57ba00
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-19 18:03:50 -0700

    alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'z'; alpha <= 'a'; alpha--)
+{
+putchar(alpha);
+}
+
+
+putchar('\n');
+
+return (0);
+}
