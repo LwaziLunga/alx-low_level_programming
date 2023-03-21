e403cb20e1c2df9bae8268f30af9b360db61f525
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-21 14:41:30 -0700

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 
 /**
  * main - check the code.
- *
+ *Description:a function that prints 10 times the alphabet,
+  In lowercase, followed by a new line.
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
@@ -10,11 +11,12 @@ void print_alphabet_x10(void)
 int i;
 int a;
 
-for(a = 0; a <= 10; a++)
+for (a = 0; a <= 10; a++)
 {
-for(i = 97; i <= 122; i++)
+for (i = 97; i <= 122; i++)
 {
 _putchar(i);
+_putchar('\n');
 }
 }
 _putchar('\n');
