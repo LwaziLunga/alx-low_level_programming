938005ac0ac9f20c70e4ca7d1e6a111ac6e830cf
Lwazi Lunga <lwazilunga@gmail.com>
2023-03-22 06:38:40 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- * a function that adds two integers and returns the result
- * Return: Always 0.
- */
+*main - check the code
+*a function that adds two integers and returns the result
+*Return: Always 0.
+*/
 
 int add(int num1, int num2)
 {
 return (num1 + num2);
 _putchar('\n');
-	
+
 }
